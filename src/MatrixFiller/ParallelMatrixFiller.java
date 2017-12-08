417aa0c028d6507ca70ab54507d7f289c24f7bd0
mgmalana <mgmalana@gmail.com>
2017-12-08 22:12:04 +0800

    Merge branch 'master' of /Users/mgmalana/Documents/Github Projects/Smith-Waterman-Implementations with conflicts.

--- src/MatrixFiller/ParallelMatrixFiller.java
+++ src/MatrixFiller/ParallelMatrixFiller.java
@@ -1,9 +1,8 @@
 package MatrixFiller;
 
 import java.util.ArrayList;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.List;
+import java.util.concurrent.*;
 
 public class ParallelMatrixFiller extends MatrixFiller {
     private int kFinal;
@@ -15,64 +14,35 @@ public class ParallelMatrixFiller extends MatrixFiller {
         super(matchScore,mismatchScore,gapPenalty);
     }
 
-//    public int[][] fillMatrix (String stringA, String stringB) {
-//        int[][] matrix = new int[stringA.length() + 1][stringB.length() + 1]; // plus 1 because of the init 0
-//        ExecutorService executorService = Executors.newFixedThreadPool(2);
-//
-//        for (int i = 1; i < matrix.length; i++) {  // starts with 1 because row 1 is all 0
-//            int iFinal = i;
-//            Runnable runnable = new Runnable() {
-//                @Override
-//                public void run() {
-//                    for (int j = 1; j < matrix[iFinal].length; j++) { // starts with 1 because column 1 is all 0
-//                        fillCell(matrix, iFinal, j, stringA, stringB);
-//                    }
-//                }
-//            };
-//            executorService.submit(runnable);
-//        }
-//
-//        return matrix;
-//    }
-
-
-    public int[][] fillMatrix (String stringA, String stringB) throws InterruptedException {
+    public int[][] fillMatrix (String stringA, String stringB) {
         int[][] matrix = new int[stringA.length() + 1][stringB.length() + 1]; // plus 1 because of the init 0
-        int stringALen = stringA.length();
-        int stringBLen = stringB.length();
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
 
-        int len = stringALen < stringBLen ? stringALen : stringBLen;
-
-        Thread thread1 = new Thread(() -> {
-            for(int i = kFinal; i <= stringALen; i++) {
-                fillCell(matrix, i, kFinal, stringA, stringB);
-            }
-        });
-
-        Thread thread2 = new Thread(() -> {
-            for(int j = kFinal + 1; j <= stringBLen; j++) {
-                fillCell(matrix, kFinal, j, stringA, stringB);
-            }
-        });
-        for(int k = 1; k <= len; k++){
-            kFinal = k;
-
-            thread1.run();
-            thread2.run();
+        for (int i = 1; i < matrix.length; i++) {  // starts with 1 because row 1 is all 0
+            int iFinal = i;
+            Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                    for (int j = 1; j < matrix[iFinal].length; j++) { // starts with 1 because column 1 is all 0
+                        fillCell(matrix, iFinal, j, stringA, stringB);
+                    }
+                }
+            };
+            executorService.submit(runnable);
         }
 
         return matrix;
     }
 
 //    public int[][] fillMatrix (String stringA, String stringB) throws InterruptedException {
-//        ExecutorService executorService = Executors.newFixedThreadPool(2);
+//        ExecutorService executor = Executors.newFixedThreadPool(2);
 //        int[][] matrix = new int[stringA.length() + 1][stringB.length() + 1]; // plus 1 because of the init 0
 //        int stringALen = stringA.length();
 //        int stringBLen = stringB.length();
 //
 //        int len = stringALen < stringBLen ? stringALen : stringBLen;
 //
-//        Thread thread1 = new Thread() {
+//        Runnable r1 = new Runnable() {
 //            @Override
 //            public void run() {
 //
@@ -82,7 +52,7 @@ public class ParallelMatrixFiller extends MatrixFiller {
 //            }
 //        };
 //
-//        Thread thread2 = new Thread() {
+//        Runnable r2 = new Runnable() {
 //            @Override
 //            public void run() {
 //                for(int j = kFinal + 1; j <= stringBLen; j++) {
@@ -92,24 +62,27 @@ public class ParallelMatrixFiller extends MatrixFiller {
 //        };
 //        for(int k = 1; k <= len; k++){
 //            kFinal = k;
-//
-//            thread1.run();
-//            thread2.run();
+//            Thread t1 = new Thread(r1);
+//            t1.start();
+//            Thread t2 = new Thread(r2);
+//            t2.start();
+//            t1.join();
+//            t2.join();
 //        }
 //
 //        return matrix;
 //    }
 // two way parallel
-//    public int[][] fillMatrix (String stringA, String stringB) throws InterruptedException {
-//        ExecutorService executorService = Executors.newFixedThreadPool(6);
+//    public int[][] fillMatrix (String stringA, String stringB) throws InterruptedException, ExecutionException {
+//        ExecutorService executorService = Executors.newFixedThreadPool(2);
 //        int[][] matrix = new int[stringA.length() + 1][stringB.length() + 1]; // plus 1 because of the init 0
 //        int stringALen = stringA.length();
 //        int stringBLen = stringB.length();
 //
 //        int len = stringALen < stringBLen ? stringALen : stringBLen;
 //
-//        ArrayList<Callable<Void>> callables = new ArrayList<>();
-//
+//        List<Callable<Void>> callables = new ArrayList<>();
+//        List<Future<Void>> future;
 //        callables.add(new Callable<Void>() {
 //            @Override
 //            public Void call() throws Exception {
@@ -129,13 +102,24 @@ public class ParallelMatrixFiller extends MatrixFiller {
 //                return null;
 //            }
 //        });
+//        long start;
+//        long end;
+//        long time = 0;
 //
 //        for(int k = 1; k <= len; k++){
 //            this.kFinal = k;
-//
-//            executorService.invokeAll(callables);
+//            start = System.nanoTime();
+//            future = executorService.invokeAll(callables);
+//            end = System.nanoTime();
+//            time += end-start;
+//            for (Future f:future) { // for blocking
+//                f.get();
+//            }
 //        }
 //
+//        System.out.println("Threads Time: " + time);
+//
+//        executorService.shutdown();
 //        return matrix;
 //    }
 
