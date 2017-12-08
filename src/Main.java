6997ab67619173b604d7e08def6e00463d1293d9
mgmalana <mgmalana@gmail.com>
2017-12-08 10:58:06 +0800

    all working but slow parallel implementations huhu

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,55 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        int[][] matrix;
-//        StandardMatrixFiller standardMatrixFiller = new StandardMatrixFiller();
-//        matrix = standardMatrixFiller.fillMatrix("GGTTGACTA", "TGTTACGG");
+        long start;
+        long end;
+
+        start = System.nanoTime();
+        StandardMatrixFiller standardMatrixFiller = new StandardMatrixFiller();
+        int[][] matrix1 = standardMatrixFiller.fillMatrix("DSADASDFFFFFSDSADSADASDADSADSADSDFFFDSADASDFFFFFSDSADSADASDADSADSADSDFFFDSADASDFFFFFSDSADSADASDADSADSADSDFFFDSADASDFFFFFSDSADSADASDADSADSADSDFFFDSADASDFFFFFSDSADSADASDADSADSADSDFFFDSADASDFFFFFSDSADSADASDADSADSADSDFFF",
+                "DSADASDFFFFFSDSADSADASDADSADSADSDFFFDSADASDFFFFFSDSADSADASDADSADSADSDFFFDSADASDFFFFFSDSADSADASDADSADSADSDFFFDSADASDFFFFFSDSADSADASDADSADSADSDFFFDSADASDFFFFFSDSADSADASDADSADSADSDFFFDSADASDFFFFFSDSADSADASDADSADSADSDFFF");
+        end = System.nanoTime();
+        System.out.println("Standard Matrix Filler Time: " + (end - start));
+
+        start = System.nanoTime();
+        ParallelMatrixFiller parallelMatrixFiller = new ParallelMatrixFiller();
+        int[][] matrix2 = parallelMatrixFiller.fillMatrix("DSADASDFFFFFSDSADSADASDADSADSADSDFFFDSADASDFFFFFSDSADSADASDADSADSADSDFFFDSADASDFFFFFSDSADSADASDADSADSADSDFFFDSADASDFFFFFSDSADSADASDADSADSADSDFFFDSADASDFFFFFSDSADSADASDADSADSADSDFFFDSADASDFFFFFSDSADSADASDADSADSADSDFFF",
+                "DSADASDFFFFFSDSADSADASDADSADSADSDFFFDSADASDFFFFFSDSADSADASDADSADSADSDFFFDSADASDFFFFFSDSADSADASDADSADSADSDFFFDSADASDFFFFFSDSADSADASDADSADSADSDFFFDSADASDFFFFFSDSADSADASDADSADSADSDFFFDSADASDFFFFFSDSADSADASDADSADSADSDFFF");
+        end = System.nanoTime();
+        System.out.println("Parallel Matrix Filler Time: " + (end - start));
+
+//        System.out.println("matrix1: ");
+//        Main.printMatrix(matrix1);
+//        System.out.println("matrix2: ");
+//        Main.printMatrix(matrix2);
+
+        System.out.println("Is Matrix equal: " + Main.isMatrixSame(matrix1,matrix2));
+
+//        start = System.nanoTime();
+//        ParallelMatrixFiller parallelMatrixFiller = new ParallelMatrixFiller();
+//        matrix = parallelMatrixFiller.fillMatrix("GGTTGACTA",
+//                "GGTTGACTA");
+//        end = System.nanoTime();
+
+//        System.out.println("Parallel Matrix Filler Time: " + (end - start));
 
 //        StandardMatrixFiller standardMatrixFiller = new StandardMatrixFiller(5, -3, -4);
 //        matrix = standardMatrixFiller.fillMatrix("GACTTAC", "CGTGAATTCAT");
-
-        ParallelMatrixFiller parallelMatrixFiller = new ParallelMatrixFiller();
-        matrix = parallelMatrixFiller.fillMatrix("GGTTGACTA", "TGTTACGG");
-
-
-        Main.printMatrix(matrix);
     }
 
     public static void printMatrix(int [][] matrix) {
         System.out.println(Arrays.deepToString(matrix).replace("], ", "]\n").replace("[[", "[").replace("]]", "]"));
+    }
+
+    public static boolean isMatrixSame(int[][] matrix1, int[][] matrix2) {
+        for (int i = 1; i < matrix1.length; i++) {  // starts with 1 because row 1 is all 0
+            for (int j = 1; j < matrix1[i].length; j++) { // starts with 1 because column 1 is all 0
+                if(matrix1[i][j] != matrix2[i][j]){
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
--- src/MatrixFiller/ParallelMatrixFiller.java
+++ src/MatrixFiller/ParallelMatrixFiller.java
@@ -6,6 +6,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ParallelMatrixFiller extends MatrixFiller {
+    private int kFinal;
     public ParallelMatrixFiller() {
         super();
     }
@@ -14,66 +15,191 @@ public class ParallelMatrixFiller extends MatrixFiller {
         super(matchScore,mismatchScore,gapPenalty);
     }
 
-    public int[][] fillMatrix (String stringA, String stringB) throws InterruptedException {
-        ExecutorService executorService = Executors.newFixedThreadPool(10);
+//    public int[][] fillMatrix (String stringA, String stringB) {
+//        int[][] matrix = new int[stringA.length() + 1][stringB.length() + 1]; // plus 1 because of the init 0
+//        ExecutorService executorService = Executors.newFixedThreadPool(2);
+//
+//        for (int i = 1; i < matrix.length; i++) {  // starts with 1 because row 1 is all 0
+//            int iFinal = i;
+//            Runnable runnable = new Runnable() {
+//                @Override
+//                public void run() {
+//                    for (int j = 1; j < matrix[iFinal].length; j++) { // starts with 1 because column 1 is all 0
+//                        fillCell(matrix, iFinal, j, stringA, stringB);
+//                    }
+//                }
+//            };
+//            executorService.submit(runnable);
+//        }
+//
+//        return matrix;
+//    }
 
+
+    public int[][] fillMatrix (String stringA, String stringB) throws InterruptedException {
+        int[][] matrix = new int[stringA.length() + 1][stringB.length() + 1]; // plus 1 because of the init 0
         int stringALen = stringA.length();
         int stringBLen = stringB.length();
-        int[][] matrix = new int[stringALen + 1][stringBLen + 1]; // plus 1 because of the init 0
-        int diagRow = 1;
-        int diagCol = 2;
 
-        while (diagRow <= stringALen) { // loop in going down per row
-            int i = diagRow;
-            ArrayList<Callable<Void>> callables = new ArrayList<>();
+        int len = stringALen < stringBLen ? stringALen : stringBLen;
 
-            for (int j = 1; j <= diagRow; j++) { // loop in traversing the diagonal line
-                final int iFinal = i;
-                final int jFinal = j;
-
-                callables.add(new Callable<Void>() {
-                    public Void call() throws Exception {
-                        fillCell(matrix, iFinal, jFinal, stringA, stringB);
-                        return null;
-                    }
-                });
-                i--;
-                if (j == stringBLen) { // if already reached the edge
-                    break;
-                }
+        Thread thread1 = new Thread(() -> {
+            for(int i = kFinal; i <= stringALen; i++) {
+                fillCell(matrix, i, kFinal, stringA, stringB);
             }
+        });
 
-            executorService.invokeAll(callables);
-            diagRow++;
-        }
-
-        while (diagCol <= stringBLen) {
-            int i = stringALen;
-            ArrayList<Callable<Void>> callables = new ArrayList<>();
-
-            for (int j = diagCol; j <= stringBLen; j++) { // loop in traversing the diagonal line
-                final int iFinal = i;
-                final int jFinal = j;
-
-                callables.add(new Callable<Void>() {
-                    public Void call() throws Exception {
-                        fillCell(matrix, iFinal, jFinal, stringA, stringB);
-                        return null;
-                    }
-                });
-
-                i--;
-
-                if(i == 0) {
-                    break;
-                }
+        Thread thread2 = new Thread(() -> {
+            for(int j = kFinal + 1; j <= stringBLen; j++) {
+                fillCell(matrix, kFinal, j, stringA, stringB);
             }
-            executorService.invokeAll(callables);
+        });
+        for(int k = 1; k <= len; k++){
+            kFinal = k;
 
-            diagCol++;
+            thread1.run();
+            thread2.run();
         }
-
 
         return matrix;
     }
+
+//    public int[][] fillMatrix (String stringA, String stringB) throws InterruptedException {
+//        ExecutorService executorService = Executors.newFixedThreadPool(2);
+//        int[][] matrix = new int[stringA.length() + 1][stringB.length() + 1]; // plus 1 because of the init 0
+//        int stringALen = stringA.length();
+//        int stringBLen = stringB.length();
+//
+//        int len = stringALen < stringBLen ? stringALen : stringBLen;
+//
+//        Thread thread1 = new Thread() {
+//            @Override
+//            public void run() {
+//
+//                for(int i = kFinal; i <= stringALen; i++) {
+//                    fillCell(matrix, i, kFinal, stringA, stringB);
+//                }
+//            }
+//        };
+//
+//        Thread thread2 = new Thread() {
+//            @Override
+//            public void run() {
+//                for(int j = kFinal + 1; j <= stringBLen; j++) {
+//                    fillCell(matrix, kFinal, j, stringA, stringB);
+//                }
+//            }
+//        };
+//        for(int k = 1; k <= len; k++){
+//            kFinal = k;
+//
+//            thread1.run();
+//            thread2.run();
+//        }
+//
+//        return matrix;
+//    }
+// two way parallel
+//    public int[][] fillMatrix (String stringA, String stringB) throws InterruptedException {
+//        ExecutorService executorService = Executors.newFixedThreadPool(6);
+//        int[][] matrix = new int[stringA.length() + 1][stringB.length() + 1]; // plus 1 because of the init 0
+//        int stringALen = stringA.length();
+//        int stringBLen = stringB.length();
+//
+//        int len = stringALen < stringBLen ? stringALen : stringBLen;
+//
+//        ArrayList<Callable<Void>> callables = new ArrayList<>();
+//
+//        callables.add(new Callable<Void>() {
+//            @Override
+//            public Void call() throws Exception {
+//                for(int i = kFinal; i <= stringALen; i++) {
+//                    fillCell(matrix, i, kFinal, stringA, stringB);
+//                }
+//                return null;
+//            }
+//        });
+//
+//        callables.add(new Callable<Void>() {
+//            @Override
+//            public Void call() throws Exception {
+//                for(int j = kFinal + 1; j <= stringBLen; j++) {
+//                    fillCell(matrix, kFinal, j, stringA, stringB);
+//                }
+//                return null;
+//            }
+//        });
+//
+//        for(int k = 1; k <= len; k++){
+//            this.kFinal = k;
+//
+//            executorService.invokeAll(callables);
+//        }
+//
+//        return matrix;
+//    }
+
+//    // this is the diagonal threading. not worth it magparallel huhu
+//    public int[][] fillMatrix (String stringA, String stringB) throws InterruptedException {
+//        ExecutorService executorService = Executors.newFixedThreadPool(20);
+//
+//        int stringALen = stringA.length();
+//        int stringBLen = stringB.length();
+//        int[][] matrix = new int[stringALen + 1][stringBLen + 1]; // plus 1 because of the init 0
+//        int diagRow = 1;
+//        int diagCol = 2;
+//
+//        while (diagRow <= stringALen) { // loop in going down per row
+//            int i = diagRow;
+//            ArrayList<Callable<Void>> callables = new ArrayList<>();
+//
+//            for (int j = 1; j <= diagRow; j++) { // loop in traversing the diagonal line
+//                final int iFinal = i;
+//                final int jFinal = j;
+//
+//                callables.add(new Callable<Void>() {
+//                    public Void call() throws Exception {
+//                        fillCell(matrix, iFinal, jFinal, stringA, stringB);
+//                        return null;
+//                    }
+//                });
+//                i--;
+//                if (j == stringBLen) { // if already reached the edge
+//                    break;
+//                }
+//            }
+//
+//            executorService.invokeAll(callables);
+//            diagRow++;
+//        }
+//
+//        while (diagCol <= stringBLen) {
+//            int i = stringALen;
+//            ArrayList<Callable<Void>> callables = new ArrayList<>();
+//
+//            for (int j = diagCol; j <= stringBLen; j++) { // loop in traversing the diagonal line
+//                final int iFinal = i;
+//                final int jFinal = j;
+//
+//                callables.add(new Callable<Void>() {
+//                    public Void call() throws Exception {
+//                        fillCell(matrix, iFinal, jFinal, stringA, stringB);
+//                        return null;
+//                    }
+//                });
+//
+//                i--;
+//
+//                if(i == 0) {
+//                    break;
+//                }
+//            }
+//            executorService.invokeAll(callables);
+//
+//            diagCol++;
+//        }
+//
+//
+//        return matrix;
+//    }
 }
